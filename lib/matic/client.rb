e87209c329269a3fc260352f6553aa42dee1733d
James Dullaghan <james@fldlvl.com>
2017-03-30 12:56:53 -0600

    Remove pry

--- lib/matic/client.rb
+++ lib/matic/client.rb
@@ -6,34 +6,59 @@ module Matic
                     :private_key
     end
 
-    attr_reader :post_body,
+    attr_reader :base_url,
+                :client_name,
+                :private_key,
+                :post_body,
+                :curl_method,
                 :request_method,
                 :api_endpoint
 
     def initialize(opts = {})
-      @base_url = Matic::Client.base_url || opts.fetch(:base_url, missing_argument(:key))
-      @client_name = Matic::Client.client_name || opts.fetch(:client_name, missing_argument(:client_name))
-      @private_key = Matic::Client.private_key || opts.fetch(:private_key, missing_argument(:private_key))
-      @post_body = opts.fetch(:post_body, "")
+      @base_url    = Matic::Client.base_url || opts.fetch(:base_url) { missing_argument(:base_url) }
+      @client_name = Matic::Client.client_name || opts.fetch(:client_name) { missing_argument(:client_name) }
+      @private_key = Matic::Client.private_key || opts.fetch(:private_key) { missing_argument(:private_key) }
+      @post_body   = opts.fetch(:post_body, "")
       # PROVIDE AN UPPERCASE REQUEST METHOD
-      @request_method = opts.fetch(:request_method, missing_argument(:request_method))
-      @api_endpoint = opts.fetch(:api_endpoint, missing_argument(:api_endpoint))
+      @curl_method    = opts.fetch(:curl_method) { :get }
+      @request_method = opts.fetch(:request_method) { "GET" }
+      @api_endpoint   = opts.fetch(:api_endpoint) { missing_argument(:api_endpoint) }
     end
 
-    def get(path)
+    def self.get(path)
       perform(path, :get)
     end
 
-    def post(path, body)
+    def self.post(path, body)
       perform(path, :post, body: body)
     end
 
-    def put(path, body, opts = {})
+    def self.put(path, body, opts = {})
       perform(path, :put, opts.merge(body: body))
     end
 
-    def delete(path, body = nil)
+    def self.delete(path, body = nil)
       perform(path, :delete, body: body)
+    end
+
+    def perform
+      c = Curl::Easy.new(url)
+      c.headers = default_headers
+
+      case curl_method
+      when :put, :delete
+        c.put_data = post_body
+      when :post
+        c.multipart_form_post = true
+        c.post_body = post_body
+      end
+
+      if verified?
+        c.http(curl_method)
+        c
+      else
+        raise Matic::UnexpectedResponseBody, "Cannot verify signature"
+      end
     end
 
     private
@@ -52,31 +77,19 @@ module Matic
 
       client = ::Matic::Client.new(
         post_body: opts[:body],
+        curl_method: method,
         request_method: formatted_method,
         api_endpoint: path,
       )
 
-      c = Curl::Easy.new(client.url)
-      c.headers = client.default_headers
-
-      case method
-      when :put, :delete
-        c.put_data = client.post_body
-      when :post
-        c.multipart_form_post = true
-        c.post_body = client.post_body
-      end
-
-      c.http(method)
-
-      c
+      client.perform
     end
 
-    # Unix Timestamp
+    # Full Url
     #
-    # @return [Integer]
-    def timestamp
-      @timestamp ||= Time.now.to_i
+    # @return [String]
+    def url
+      base_url + api_endpoint
     end
 
     # Secret string required for authentication
@@ -93,14 +106,29 @@ module Matic
       ].join(':')
     end
 
+    # Unix Timestamp
+    #
+    # @return [Integer]
+    def timestamp
+      @timestamp ||= Time.now.to_i
+    end
+
     # Private Key
     def key
       @key ||= OpenSSL::PKey::RSA.new(private_key)
     end
 
+    def digest
+      @digest ||= OpenSSL::Digest::SHA256.new
+    end
+
     # Signed Key
     def signature
-      @signature ||= key.sign(OpenSSL::Digest::SHA256.new, secret_string)
+      @signature ||= key.sign(digest, secret_string)
+    end
+
+    def verified?
+      key.verify(digest, signature, secret_string)
     end
 
     # API Service expects following headers along every request:
